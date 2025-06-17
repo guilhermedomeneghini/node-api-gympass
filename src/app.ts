217ed99974d74803d77f2ead5fe6bab40975947d
guilhermedomeneghini <guilherme.walmeling@gmail.com>
2025-06-16 21:01:32 -0300

    Config prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+// instanciando o prismaclient
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'irineu',
+    email: 'mail@email.com',
+  },
+})
